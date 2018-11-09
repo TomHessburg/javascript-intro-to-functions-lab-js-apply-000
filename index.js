623b48ae367c46999bfedb2fae7d6c47b5d0ab3e
Thomas Hessburg <tomhessburg@yahoo.com>
2018-11-09 03:29:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,12 +21,10 @@ function sayHiToGrandma(string){
   if(string === string.toUpperCase()){
     return "YES INDEED!";
   }
-}
-
-function sayHiToGrandma(string){
   if(string === "I love you, Grandma."){
     return "I love you, too.";
   }
 }
+
 
 
