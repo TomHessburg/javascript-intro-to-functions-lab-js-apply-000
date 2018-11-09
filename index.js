fd6bfd7c9a034611403a9b474843dac1e19731e5
Thomas Hessburg <tomhessburg@yahoo.com>
2018-11-09 03:28:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string === string.toLowerCase()){
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
   if(string === string.toUpperCase()){
-    return "YES INDEED!"
+    return "YES INDEED!";
   }
 }
 
 function sayHiToGrandma(string){
   if(string === "I love you, Grandma."){
-    return 
+    return "I love you, too.";
   }
 }
 
